7f0b01e02ae1096761f7db383a52769b2e7211ec
Marykathriner <91670061+Marykathriner@users.noreply.github.com>
2021-11-25 15:01:27 +0100

    mobile view

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ function hidePopup () {
 	var popup = document.getElementById('popup');
 	popup.classList.add('show') 
 }
-
 function hideBlanket () {
     var blanket = document.getElementById('blanket');
     blanket.classList.remove('blanket')

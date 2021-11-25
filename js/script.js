8f9003daf1c8d6ec973fa208182a5f139ae106cb
Marykathriner <91670061+Marykathriner@users.noreply.github.com>
2021-11-25 14:41:55 +0100

    mobile view
    Footer, nav, text colum

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function hidePopup () {
+	var popup = document.getElementById('popup');
+	popup.classList.add('show') 
+}
+
+function hideBlanket () {
+    var blanket = document.getElementById('blanket');
+    blanket.classList.remove('blanket')
+}
